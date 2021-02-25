ca1ab317db58ee4aa0e5a938c39c1e009ee2d432
Shane McDonough <shanemcdman@gmail.com>
2021-02-24 16:23:14 -0800

    Create fn get_char

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
-use crossterm::{execute, cursor::MoveTo};
+use crossterm::{
+    cursor::MoveTo,
+    event::{read, Event, KeyCode},
+    execute,
+};
 use std::io::{stdout, Write};
 struct Coord {
     x: i32,
@@ -20,5 +24,16 @@ fn move_cursor(x: u16, y: u16) {
     execute!(stdout(), MoveTo(x, y));
 }
 
+fn get_char() -> KeyCode {
+    loop {
+        match read() {
+            Ok(Event::Key(event)) => break event.code,
+            Err(_) => panic!("Could not read!"),
+            _ => (),
+        }
+    }
+}
+
 fn main() {
+    print!("{:?}", get_char());
 }
